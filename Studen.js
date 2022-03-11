e95d44f1696995cd6622c75d6887427b8c9e0df5
unknown <knts2nttn@gmail.com>
2022-03-11 14:20:09 +0700

    lamweb

---
+++ Studen.js
@@ -0,0 +1,25 @@
+class Studen {
+    constructor(id,name,age) {
+        this.id = id
+        this.name = name
+        this.age = age
+    }
+    getId(){
+        return this.id
+    }
+    setId(id){
+        this.id = id
+    }
+    getName(){
+        return this.name
+    }
+    setName(name){
+        this.name = name
+    }
+    getAge(){
+        return this.age
+    }
+    setAge(age){
+        this.age = age
+    }
+}
